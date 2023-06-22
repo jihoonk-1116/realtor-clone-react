bce14681a44fda834c380258077e46841c473a64
jihoonk-1116 <jihoonk1116@gmail.com>
2023-06-22 12:13:48 -0400

    Completed the header component and make it dynamic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
-
-import { useLocation,useNavigate } from "react-router-dom"
+import { useState, useEffect } from 'react';
+import {getAuth, onAuthStateChanged} from "firebase/auth";
+import { useLocation,useNavigate } from "react-router-dom";
 
 
 export default function Header() {
-
+  const [pageState, setPageState] = useState("Sign in");
   const location = useLocation();
   const navigate = useNavigate();
+  const auth = getAuth();
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if(user){
+        setPageState("Profile");
+      }else{
+        setPageState("Sign in");
+      }
+    })
+  }, [auth])
   function pathMatchRoute(route){
     if(route === location.pathname){
       return true
@@ -42,10 +52,10 @@ export default function Header() {
                 <li
                   className={
                     `cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent 
-                    ${pathMatchRoute("/sign-in") && "!text-black !border-b-red-500"}`
+                    ${(pathMatchRoute("/sign-in") || pathMatchRoute("/profile")) && "!text-black !border-b-red-500"}`
                   }
-                  onClick={() => navigate("/sign-in")}
-                >Sign in</li>
+                  onClick={() => navigate("/profile")}
+                >{pageState}</li>
               </ul>
             </div>
         </header>

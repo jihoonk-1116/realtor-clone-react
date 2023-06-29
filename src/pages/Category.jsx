a869b16a44a6e65445da40f3918a5af626bfec86
jihoonk-1116 <jihoonk1116@gmail.com>
2023-06-29 15:16:00 -0400

    implemented category page

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,20 +1,22 @@
 import { collection, getDocs, limit, orderBy, query, startAfter, where } from 'firebase/firestore';
 import React from 'react'
 import { useState, useEffect } from 'react'
+import { useParams } from 'react-router';
 import { toast } from 'react-toastify';
 import ListingItem from '../components/ListingItem';
 import Spinner from '../components/Spinner';
 import { db } from '../firebase';
 
-export default function Offers() {
+export default function Category() {
   const [listings, setListings] = useState(null);
   const [loading, setLoading] = useState(true);
   const [lastFetchedListing, setLastFetchListing] = useState(null);
+  const params = useParams();
   useEffect(() => {
     async function fetchListings(){
       try {
         const listingRef = collection(db, "listings");
-        const q = query(listingRef, where("offer", "==", true), orderBy("timestamp", "desc"), limit(8));
+        const q = query(listingRef, where("type", "==", params.categoryName), orderBy("timestamp", "desc"), limit(8));
         const querySnap = await getDocs(q);
         const lastVisible = querySnap.docs[querySnap.docs.length-1] // get last doc
         setLastFetchListing(lastVisible);
@@ -33,12 +35,12 @@ export default function Offers() {
       }
     }
     fetchListings();
-  }, [])
+  }, [params.categoryName])
   async function onFetchMoreListing(){
     try {
       const listingRef = collection(db, "listings");
       const q = query(listingRef, 
-                      where("offer", "==", true), 
+                      where("type", "==", params.categoryName), 
                       orderBy("timestamp", "desc"), 
                       startAfter(lastFetchedListing),
                       limit(4)
@@ -62,13 +64,15 @@ export default function Offers() {
   }
   return (
     <div className='max-w-6xl mx-auto px-3'>
-      <h1 className="text-3xl text-center mt-6 font-bold mb-6">Offers</h1>
+      <h1 className="text-3xl text-center mt-6 font-bold mb-6">
+          {params.categoryName === "rent" ? "Places for rent" : "Places for sale"}
+      </h1>
       {loading ? (
           <Spinner/>
       ) : listings && listings.length > 0 ? (
         <>
           <main>
-            <ul className='sm:grid sm:grid-col-2 lg:grid -cols-3 xl:grid-cols-4 2xl:grid-cols-5'>
+            <ul className='sm:grid sm:grid-cols-2 lg:grid -cols-3 xl:grid-cols-4 2xl:grid-cols-5'>
               {listings.map((listing) => (
                 <ListingItem 
                   key={listing.id}
@@ -93,8 +97,15 @@ export default function Offers() {
           }
         </>
       ) : (
-        <p>There are no current offers</p>
+        <p>
+            There are no current offers{" "}
+            {params.categoryName === "rent"
+                ? "places for rent"
+                : "places for sale"
+            }
+        </p>
       )}
     </div>
     )
 }
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,149 @@
+
+
+import { collection, orderBy, query, limit, getDocs, where } from "firebase/firestore";
+import { useState, useEffect } from "react"
+import { Link } from "react-router-dom";
+import ListingItem from "../components/ListingItem";
 import Slider from "../components/Slider"
+import { db } from "../firebase";
+
+
 export default function Home() {
+  // offers
+  const [offerListings, setOfferListings] = useState(null);
+  useEffect(()=>{
+    async function fetchListings(){
+      try{
+        //get reference
+        const listingsRef = collection(db, "listings");
+        const q = query(listingsRef, where("offer", "==", true), orderBy("timestamp", "desc")
+                        ,limit(4)
+        );
+        const querySnap = await getDocs(q);
+        const listings = [];
+        querySnap.forEach((doc) => {
+          return listings.push({
+            id: doc.id,
+            data: doc.data(),
+
+          });
+        });
+        setOfferListings(listings);
+      }catch(err){
+        console.log(err);
+      }
+    }
+    fetchListings();
+  }, []) //using useEffect without dependency is for calling it once 
+  // rent
+  const [rentListings, setRentListings] = useState(null);
+  useEffect(()=>{
+    async function fetchListings(){
+      try{
+        //get reference
+        const listingsRef = collection(db, "listings");
+        const q = query(listingsRef, where("type", "==", "rent"), orderBy("timestamp", "desc")
+                        ,limit(4)
+        );
+        const querySnap = await getDocs(q);
+        const listings = [];
+        querySnap.forEach((doc) => {
+          return listings.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+        });
+        setRentListings(listings);
+        console.log(listings);
+      }catch(err){
+        console.log(err);
+      }
+    }
+    fetchListings();
+  }, []) //using useEffect without dependency is for calling it once 
+  // sale
+  const [saleListings, setSaleListings] = useState(null);
+  useEffect(()=>{
+    async function fetchListings(){
+      try{
+        //get reference
+        const listingsRef = collection(db, "listings");
+        const q = query(listingsRef, where("type", "==", "sale"), orderBy("timestamp", "desc")
+                        ,limit(4)
+        );
+        const querySnap = await getDocs(q);
+        const listings = [];
+        querySnap.forEach((doc) => {
+          return listings.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+        });
+        setSaleListings(listings);
+        console.log(listings);
+      }catch(err){
+        console.log(err);
+      }
+    }
+    fetchListings();
+  }, []) //using useEffect without dependency is for calling it once 
   return (
     <div>
       <Slider/>
+      {/* offer */}
+      <div className="max-w-6xl mx-auto pt-4 space-y-6">
+        {offerListings && offerListings.length > 0 && (
+          <div className="m-2 mb-6">
+            <h2 className="px-3 text-2xl mt-6 font-semibold">Recent Offers</h2>
+            <Link to="/offers">
+              <p className="px-3 text-sm text-blue-600 hover:text-blue-800 transition
+                            duration-150 ease-in-out
+              ">Show more offers</p>
+            </Link>
+            <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+              {offerListings.map((listing) => (
+                <ListingItem key={listing.id} listing = {listing.data} id={listing.id} />
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+      {/* rent */}
+      <div className="max-w-6xl mx-auto pt-4 space-y-6">
+        {saleListings && saleListings.length > 0 && (
+          <div className="m-2 mb-6">
+            <h2 className="px-3 text-2xl mt-6 font-semibold">Places for sale</h2>
+            <Link to="/category/sale">
+              <p className="px-3 text-sm text-blue-600 hover:text-blue-800 transition
+                            duration-150 ease-in-out
+              ">Show more places for sale</p>
+            </Link>
+            <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+              {saleListings.map((listing) => (
+                <ListingItem key={listing.id} listing = {listing.data} id={listing.id} />
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+      {/* sale */}
+      <div className="max-w-6xl mx-auto pt-4 space-y-6">
+        {rentListings && rentListings.length > 0 && (
+          <div className="m-2 mb-6">
+            <h2 className="px-3 text-2xl mt-6 font-semibold">Places for rent</h2>
+            <Link to="/category/rent">
+              <p className="px-3 text-sm text-blue-600 hover:text-blue-800 transition
+                            duration-150 ease-in-out
+              ">Show more places for rent</p>
+            </Link>
+            <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+              {rentListings.map((listing) => (
+                <ListingItem key={listing.id} listing = {listing.data} id={listing.id} />
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
